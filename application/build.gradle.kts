7ae4fbc8f034480652c787f1cdd215a539cda402
Celso Taliatelli <celsotaliatelli@hotmail.com>
2024-02-06 19:33:34 -0300

    add vavr

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -9,6 +9,9 @@ repositories {
 }
 
 dependencies {
+
+    implementation ("io.vavr:vavr:0.10.4")
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.mockito:mockito-junit-jupiter:4.5.1")
