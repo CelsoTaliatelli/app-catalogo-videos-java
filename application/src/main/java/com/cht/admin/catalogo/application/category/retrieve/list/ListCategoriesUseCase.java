56233a38644a0ba90dfc25d13a770c11a5ff6153
Celso Taliatelli <celsotaliatelli@hotmail.com>
2024-02-20 20:43:29 -0300

    add list categories usecase

---
+++ application/src/main/java/com/cht/admin/catalogo/application/category/retrieve/list/ListCategoriesUseCase.java
@@ -0,0 +1,8 @@
+package com.cht.admin.catalogo.application.category.retrieve.list;
+
+import com.cht.admin.catalogo.application.UseCase;
+import com.cht.admin.catalogo.domain.pagination.Pagination;
+import com.cht.admin.catalogo.domain.pagination.SearchQuery;
+
+public abstract class ListCategoriesUseCase extends UseCase<SearchQuery, Pagination<CategoryListOutput>> {
+}
