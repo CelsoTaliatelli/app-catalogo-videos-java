b211950a95df423d69df4838898221914bc0f1f5
Celso Taliatelli <celsotaliatelli@hotmail.com>
2024-02-26 16:41:18 -0300

    configurando profiles do springboot

--- infrastructure/src/main/java/com/cht/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/cht/admin/catalogo/infrastructure/Main.java
@@ -4,11 +4,15 @@ import com.cht.admin.catalogo.application.UseCase;
 import com.cht.admin.catalogo.infrastructure.configuration.WebServerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
+
+import static org.springframework.core.env.AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.setProperty(DEFAULT_PROFILES_PROPERTY_NAME,"development");
         SpringApplication.run(WebServerConfig.class,args);
     }
 }
