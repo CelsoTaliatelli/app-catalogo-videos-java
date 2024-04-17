50465f62b590079250b26aabd95c58b7569d3296
Celso Taliatelli <celsotaliatelli@hotmail.com>
2024-04-17 16:15:34 -0300

    criando entidades JPA e migrations

---
+++ infrastructure/src/main/java/com/cht/admin/catalogo/infrastructure/genre/GenreMySQLGateway.java
@@ -0,0 +1,83 @@
+package com.cht.admin.catalogo.infrastructure.genre;
+
+import com.cht.admin.catalogo.domain.Genre.Genre;
+import com.cht.admin.catalogo.domain.Genre.GenreGateway;
+import com.cht.admin.catalogo.domain.Genre.GenreID;
+import com.cht.admin.catalogo.domain.pagination.Pagination;
+import com.cht.admin.catalogo.domain.pagination.SearchQuery;
+import com.cht.admin.catalogo.infrastructure.genre.persistence.GenreJpaEntity;
+import com.cht.admin.catalogo.infrastructure.genre.persistence.GenreRepository;
+import com.cht.admin.catalogo.infrastructure.utils.SpecificationUtils;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.Objects;
+import java.util.Optional;
+
+import static org.springframework.data.jpa.domain.Specification.where;
+
+public class GenreMySQLGateway implements GenreGateway {
+
+    private final GenreRepository genreRepository;
+
+    public GenreMySQLGateway(final GenreRepository genreRepository) {
+        this.genreRepository = Objects.requireNonNull(genreRepository);
+    }
+    @Override
+    public Genre create(Genre aGenre) {
+        return save(aGenre);
+    }
+
+    @Override
+    public void deleteById(GenreID anId) {
+        final var aGenreId = anId.getValue();
+        if (this.genreRepository.existsById(aGenreId)) {
+            this.genreRepository.deleteById(aGenreId);
+        }
+    }
+
+    @Override
+    public Optional<Genre> findById(GenreID anId) {
+        return this.genreRepository.findById(anId.getValue())
+                .map(GenreJpaEntity::toAggregate);
+    }
+
+    @Override
+    public Genre update(Genre aGenre) {
+        return save(aGenre);
+    }
+
+    @Override
+    public Pagination<Genre> findAll(SearchQuery aQuery) {
+        final var page = PageRequest.of(
+                aQuery.page(),
+                aQuery.perPage(),
+                Sort.by(Sort.Direction.fromString(aQuery.direction()), aQuery.sort())
+        );
+
+        final var where = Optional.ofNullable(aQuery.terms())
+                .filter(str -> !str.isBlank())
+                .map(this::assembleSpecification)
+                .orElse(null);
+
+        final var pageResult =
+                this.genreRepository.findAll(where(where), page);
+
+        return new Pagination<>(
+                pageResult.getNumber(),
+                pageResult.getSize(),
+                pageResult.getTotalElements(),
+                pageResult.map(GenreJpaEntity::toAggregate).toList()
+        );
+    }
+
+    private Genre save(final Genre genre) {
+        return this.genreRepository.save(GenreJpaEntity.from(genre))
+                .toAggregate();
+    }
+
+    private Specification<GenreJpaEntity> assembleSpecification(final String terms) {
+        return SpecificationUtils.like("name", terms);
+    }
+}
