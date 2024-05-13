5ced60c504adb064918a331ac9ed8c02cf29c7e9
Celso Taliatelli <celsotaliatelli@hotmail.com>
2024-05-13 20:52:46 -0300

    cobrindo cenários de testes entidade  video

---
+++ domain/src/main/java/com/cht/admin/catalogo/domain/video/VideoSearchQuery.java
@@ -0,0 +1,11 @@
+package com.cht.admin.catalogo.domain.video;
+
+public record VideoSearchQuery(
+        int page,
+        int perPage,
+        String terms,
+        String sort,
+        String direction
+) {
+
+}
