a09bdef858493c818ac76fb90ab12ee32be09190
Celso Taliatelli <celsotaliatelli@hotmail.com>
2024-06-07 17:06:22 -0300

    add UploadMediaUseCase

---
+++ application/src/main/java/com/cht/admin/catalogo/application/video/media/upload/UploadMediaOutput.java
@@ -0,0 +1,13 @@
+package com.cht.admin.catalogo.application.video.media.upload;
+
+import com.cht.admin.catalogo.domain.video.Video;
+import com.cht.admin.catalogo.domain.video.VideoMediaType;
+
+public record UploadMediaOutput(
+        String id,
+        VideoMediaType videoMediaType
+) {
+    public static UploadMediaOutput with(final Video aVideo, final VideoMediaType aType){
+        return new UploadMediaOutput(aVideo.getId().getValue(),aType);
+    }
+}
---
+++ application/src/main/java/com/cht/admin/catalogo/application/video/media/upload/UploadMediaUseCase.java
@@ -0,0 +1,5 @@
+package com.cht.admin.catalogo.application.video.media.upload;
+import com.cht.admin.catalogo.application.UseCase;
+
+public abstract class UploadMediaUseCase extends UseCase<UploadMediaCommand, UploadMediaOutput> {
+}
