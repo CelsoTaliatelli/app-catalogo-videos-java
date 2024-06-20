6a925e5978e0e81a6b1d81463a3db9c3613dd873
Celso Taliatelli <celsotaliatelli@hotmail.com>
2024-06-20 16:23:41 -0300

    criando a facade eventService

---
+++ infrastructure/src/main/java/com/cht/admin/catalogo/infrastructure/services/EventService.java
@@ -0,0 +1,5 @@
+package com.cht.admin.catalogo.infrastructure.services;
+
+public interface EventService {
+    void send(Object event);
+}
---
+++ infrastructure/src/main/java/com/cht/admin/catalogo/infrastructure/services/local/InMemoryEventService.java
@@ -0,0 +1,15 @@
+package com.cht.admin.catalogo.infrastructure.services.local;
+
+import com.cht.admin.catalogo.infrastructure.configuration.json.Json;
+import com.cht.admin.catalogo.infrastructure.services.EventService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class InMemoryEventService implements EventService {
+    private static final Logger LOG = LoggerFactory.getLogger(InMemoryEventService.class);
+
+    @Override
+    public void send(Object event) {
+        LOG.info("Event was observed: {}", Json.writeValueAsString(event));
+    }
+}
