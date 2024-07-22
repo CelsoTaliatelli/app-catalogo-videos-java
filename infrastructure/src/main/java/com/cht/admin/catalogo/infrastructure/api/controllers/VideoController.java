2a6742cafdb839e0cb0e2220b1e2448ee819687c
Celso Taliatelli <celsotaliatelli@hotmail.com>
2024-07-22 15:40:27 -0300

    add api de videos

---
+++ infrastructure/src/main/java/com/cht/admin/catalogo/infrastructure/api/controllers/VideoController.java
@@ -0,0 +1,127 @@
+package com.cht.admin.catalogo.infrastructure.api.controllers;
+
+import com.cht.admin.catalogo.application.video.create.CreateVideoCommand;
+import com.cht.admin.catalogo.application.video.create.CreateVideoUseCase;
+import com.cht.admin.catalogo.application.video.media.get.GetMediaUseCase;
+import com.cht.admin.catalogo.application.video.media.upload.UploadMediaUseCase;
+import com.cht.admin.catalogo.domain.video.Resource;
+import com.cht.admin.catalogo.infrastructure.api.VideoAPI;
+import com.cht.admin.catalogo.infrastructure.utils.HashingUtils;
+import com.cht.admin.catalogo.infrastructure.video.models.CreateVideoRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.net.URI;
+import java.util.Objects;
+import java.util.Set;
+
+@RestController
+public class VideoController implements VideoAPI {
+
+    private final CreateVideoUseCase createVideoUseCase;
+    //private final GetVideoByIdUseCase getVideoByIdUseCase;
+    //private final UpdateVideoUseCase updateVideoUseCase;
+    //private final DeleteVideoUseCase deleteVideoUseCase;
+    //private final ListVideosUseCase listVideosUseCase;
+    private final GetMediaUseCase getMediaUseCase;
+    private final UploadMediaUseCase uploadMediaUseCase;
+
+    public VideoController(
+            final CreateVideoUseCase createVideoUseCase,
+            //final GetVideoByIdUseCase getVideoByIdUseCase,
+            //final UpdateVideoUseCase updateVideoUseCase,
+            //final DeleteVideoUseCase deleteVideoUseCase,
+            //final ListVideosUseCase listVideosUseCase,
+            final GetMediaUseCase getMediaUseCase,
+            final UploadMediaUseCase uploadMediaUseCase
+    ) {
+        this.createVideoUseCase = Objects.requireNonNull(createVideoUseCase);
+        //this.getVideoByIdUseCase = Objects.requireNonNull(getVideoByIdUseCase);
+        //this.updateVideoUseCase = Objects.requireNonNull(updateVideoUseCase);
+        //this.deleteVideoUseCase = Objects.requireNonNull(deleteVideoUseCase);
+        //this.listVideosUseCase = Objects.requireNonNull(listVideosUseCase);
+        this.getMediaUseCase = Objects.requireNonNull(getMediaUseCase);
+        this.uploadMediaUseCase = Objects.requireNonNull(uploadMediaUseCase);
+    }
+
+    @Override
+    public ResponseEntity<?> createFull(
+            final String aTitle,
+            final String aDescription,
+            final Integer launchedAt,
+            final Double aDuration,
+            final Boolean wasOpened,
+            final Boolean wasPublished,
+            final String aRating,
+            final Set<String> categories,
+            final Set<String> castMembers,
+            final Set<String> genres,
+            final MultipartFile videoFile,
+            final MultipartFile trailerFile,
+            final MultipartFile bannerFile,
+            final MultipartFile thumbFile,
+            final MultipartFile thumbHalfFile
+    ) {
+        final var aCmd = CreateVideoCommand.with(
+                aTitle,
+                aDescription,
+                launchedAt,
+                aDuration,
+                wasOpened,
+                wasPublished,
+                aRating,
+                categories,
+                genres,
+                castMembers,
+                resourceOf(videoFile),
+                resourceOf(trailerFile),
+                resourceOf(bannerFile),
+                resourceOf(thumbFile),
+                resourceOf(thumbHalfFile)
+        );
+
+        final var output = this.createVideoUseCase.execute(aCmd);
+
+        return ResponseEntity.created(URI.create("/videos/" + output.id())).body(output);
+    }
+
+    @Override
+    public ResponseEntity<?> createPartial(CreateVideoRequest payload) {
+        final var aCmd = CreateVideoCommand.with(
+                payload.title(),
+                payload.description(),
+                payload.yearLaunched(),
+                payload.duration(),
+                payload.opened(),
+                payload.published(),
+                payload.rating(),
+                payload.categories(),
+                payload.genres(),
+                payload.castMembers()
+        );
+
+        final var output = this.createVideoUseCase.execute(aCmd);
+
+        return ResponseEntity.created(URI.create("/videos/" + output.id())).body(output);
+    }
+
+
+    private Resource resourceOf(final MultipartFile part) {
+        if (part == null) {
+            return null;
+        }
+
+        try {
+            return Resource.with(
+                    part.getBytes(),
+                    HashingUtils.checksum(part.getBytes()),
+                    part.getContentType(),
+                    part.getOriginalFilename()
+            );
+        } catch (Throwable t) {
+            throw new RuntimeException(t);
+        }
+    }
+
+}
