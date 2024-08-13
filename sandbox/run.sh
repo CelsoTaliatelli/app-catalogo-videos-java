37a1f3c851f7ebc43ba0f0ff19378796c7e5f414
Celso Taliatelli <celsotaliatelli@hotmail.com>
2024-08-13 09:05:23 -0300

    add observability

--- sandbox/run.sh
+++ sandbox/run.sh
@@ -3,15 +3,15 @@ docker network create adm_videos_services
 docker network create elastic
 
 # Criar as pastas com permissões
-sudo chown root app/filebeat/filebeat.docker.yml
+sudo chown root app/filebeat/filebeat.docker.yaml
 mkdir -m 777 .docker
 mkdir -m 777 .docker/es01
 mkdir -m 777 .docker/keycloak
 mkdir -m 777 .docker/filebeat
 
-docker compose -f services/docker-compose.yml up -d
-docker compose -f elk/docker-compose.yml up -d
-#docker compose -f app/docker-compose.yml up -d
+docker compose -f services/docker-compose.yaml up -d
+docker compose -f elk/docker-compose.yaml up -d
+docker compose -f app/docker-compose.yaml up -d
 
 echo "Inicializando os containers..."
 sleep 20
